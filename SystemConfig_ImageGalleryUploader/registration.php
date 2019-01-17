9638da336cd02994ae2490c69286da05009236c5
Vlad <iwwwi2015@gmail.com>
2019-01-17 13:28:24 +0200

    Added snippet image gallery in system configuration

---
+++ SystemConfig_ImageGalleryUploader/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SystemConfig_ImageGalleryUploader',
+    __DIR__
+);
